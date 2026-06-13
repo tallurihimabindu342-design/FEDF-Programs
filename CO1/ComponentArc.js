82af6cd575ad856daee0a589d6ce48954c79f227
Talluri HimaBindu Sree <tallurihimabindu342@gmail.com>
2026-06-13 06:14:04 +0530

    Add files via upload

---
+++ CO1/ComponentArc.js
@@ -0,0 +1,15 @@
+function Header() {
+    return "Header Component";
+}
+
+function Sidebar() {
+    return "Sidebar Component";
+}
+
+function Footer() {
+    return "Footer Component";
+}
+
+console.log(Header());
+console.log(Sidebar());
+console.log(Footer());
---
+++ CO1/VirtualDOM.js
@@ -0,0 +1,13 @@
+const oldData = {
+    name: "Hima",
+    age: 20
+};
+
+const newData = {
+    name: "Hima",
+    age: 21
+};
+
+if (oldData.age !== newData.age) {
+    console.log("Update only age field");
+}
